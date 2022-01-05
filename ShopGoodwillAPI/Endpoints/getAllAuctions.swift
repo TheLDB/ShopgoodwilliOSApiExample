15f38c693922a1e9e408ab5ed6eadd92984c55b0
Landon Boles <ldb@erikboles.com>
2022-01-05 01:32:46 -0800

    Oops forgot comments

--- ShopGoodwillAPI/Endpoints/getAllAuctions.swift
+++ ShopGoodwillAPI/Endpoints/getAllAuctions.swift
@@ -51,16 +51,18 @@ class getAllAuctions {
     func getAuctions() {
         AF.request(url, method: .post, parameters: aucParams, encoding: JSONEncoding.default).responseDecodable(of: DecodableType.self) { response in
                 do {
-                    let json = try JSON(data: response.data!)
+                    let json = try JSON(data: response.data!) // Uses JSONSwifty to parse JSON
                     
-                    let specificAuctionResults = json["searchResults"]["items"]
+                    let specificAuctionResults = json["searchResults"]["items"] // Gets only the stuff we need from the results
                     
                     specificAuctionResults.forEach({
+                        // $0.1 = Value
+                        // $0.0 = Key
                         print($0.1)
                     })
                 }
                 catch {
-                    print(error)
+                    print(error) // Catch any errors (like goodwill being down)
                 }
             }
     }
